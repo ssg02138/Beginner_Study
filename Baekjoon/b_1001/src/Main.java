3dff40bc83081baad4a0b68463a40a19b940f146
MinWook5 <minuk1654@naver.com>
2023-12-20 21:27:48 +0900

    깃허브 사용법 숙지

---
+++ Baekjoon/b_1001/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1 2
+        Scanner in = new Scanner(System.in);
+
+        // 1
+        int a= in.nextInt();
+
+        // 2
+        int b= in.nextInt();
+
+        System.out.println(a-b);
+    }
+}
---
+++ MyApp1/src/AccountingIFApp.java
@@ -0,0 +1,37 @@
+public class AccountingIFApp {
+    public static void main(String[] args) {
+
+        double valueOfSuppy = Double.parseDouble(args[0]);
+        double VAT = 0.1;
+        double expenseRate = 0.3;
+        double vat = valueOfSuppy * VAT;
+        double total = valueOfSuppy + vat;
+        double expense = valueOfSuppy * expenseRate;
+        double income = valueOfSuppy - expense;
+
+        double devidend1;
+        double devidend2;
+        double devidend3;
+
+        if(income > 10000.0) {
+            devidend1 = income * 0.5;
+            devidend2 = income * 0.3;
+            devidend3 = income * 0.2;
+        } else {
+            devidend1 = income * 1;
+            devidend2 = income * 0;
+            devidend3 = income * 0;
+        }
+
+        System.out.println("Value of supply : "+ valueOfSuppy);
+        System.out.println("VAT : "+ vat);
+        System.out.println("Total : "+ total);
+        System.out.println("Expense : "+ expense);
+        System.out.println("Income : "+ income);
+        System.out.println("Dividend : "+ devidend1);
+        System.out.println("Dividend : "+ devidend2);
+        System.out.println("Dividend : "+ devidend3);
+
+
+    }
+}
