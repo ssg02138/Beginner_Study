985ce1df640b16e734a9147a7f5e5843f619f6b7
MinWook5 <minuk1654@naver.com>
2023-12-24 21:14:27 +0900

    백준 반복문 2739 구구단

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ConditionalStatement a = new ConditionalStatement();
+        ForStatement b = new ForStatement();
 
-        a.b_2480();
+        b.b_2739();
     }
 }
