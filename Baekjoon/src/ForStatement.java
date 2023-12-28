01a51061ba3cbafbfeaa8bca7cc0fb6d99dc4bee
MinWook5 <minuk1654@naver.com>
2023-12-28 23:09:04 +0900

    백준 반복문 11022번 A+B-8 Bufferd 사용

--- Baekjoon/src/ForStatement.java
+++ Baekjoon/src/ForStatement.java
@@ -311,4 +311,54 @@ public class ForStatement {
         bw.close();
 
     }
+
+
+    public static void b_11022() throws IOException {
+//        문제
+//        두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//
+//        입력
+//        첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//        각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//
+//        출력
+//        각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다.
+//        x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+//
+//                예제 입력 1
+//        5
+//        1 1
+//        2 3
+//        3 4
+//        9 8
+//        5 2
+//        예제 출력 1
+//        Case #1: 1 + 1 = 2
+//        Case #2: 2 + 3 = 5
+//        Case #3: 3 + 4 = 7
+//        Case #4: 9 + 8 = 17
+//        Case #5: 5 + 2 = 7
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        int a;
+        int b;
+
+        StringTokenizer st;
+        for (int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #"+i+": ");
+            bw.write(a + " + " + b + " = ");
+            bw.write((a+b) + "\n");
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
 }
