402e3c747982e38ac6ae09d8eb4c9f0a2b1377ed
MinWook5 <minuk1654@naver.com>
2023-12-28 12:11:43 +0900

    백준 반복문 15552번 빠른 A+B Bufferd 사용

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ConditionalStatement a = new ConditionalStatement();
         ForStatement b = new ForStatement();
 
-        b.b_25314();
+        b.b_15552();
     }
 }
