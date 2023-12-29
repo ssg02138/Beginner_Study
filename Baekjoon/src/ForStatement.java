afe0640fdc1ae5684cf315708bb9d120b819c5d2
MinWook5 <minuk1654@naver.com>
2023-12-29 17:56:51 +0900

    백준 반복문 2438번 별 찍기 -2

--- Baekjoon/src/ForStatement.java
+++ Baekjoon/src/ForStatement.java
@@ -391,4 +391,39 @@ public class ForStatement {
             System.out.println();
         }
     }
+
+
+    public static void b_2439() {
+//        문제
+//        첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+//
+//        하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+//
+//        입력
+//        첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+//
+//        출력
+//        첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+//
+//                예제 입력 1
+//        5
+//        예제 출력 1
+//                *
+//               **
+//              ***
+//             ****
+//            *****
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
