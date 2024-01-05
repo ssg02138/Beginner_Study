151624205a6dd19d00ef43932f4adfb674d1336d
MinWook5 <minuk1654@naver.com>
2024-01-05 19:02:30 +0900

    별도 프로젝트 업로드

--- BankApp/src/Main.java
+++ BankApp/src/Main.java
@@ -9,6 +9,5 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("test");
-        System.out.println("test");
     }
 }
