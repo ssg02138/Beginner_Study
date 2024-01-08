b20f306e78be12b29cc28aac6f195963d0cd335a
MinWook5 <minuk1654@naver.com>
2024-01-08 21:23:40 +0900

    BankApp 만들기 Account 클래스 생성

--- BankApp/src/Account.java
+++ BankApp/src/Account.java
@@ -1,24 +1,15 @@
 import java.util.Scanner;
 public class Account {
+    private String accountNubmer;
+    private String name;
+    private String password;
+    private int balance;
 
-    static Account[] accounts = new Account[100];
-    static int index = 0;
-
-    private static void createAccount() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("계좌번호: ");
-        String accountNumber = sc.nextLine();
-
-        System.out.println("이름: ");
-        String name = sc.nextLine();
-
-        System.out.println("비밀번호: ");
-        String password = sc.nextLine();
-
-        System.out.println("잔고: ");
-        int balance = Integer.parseInt(sc.nextLine());
-
-        Account account = new Account();
-        accounts[index++] = account;
+    public Account(String accountNubmer, String name, String password, int balance) {
+        this.accountNubmer = accountNubmer;
+        this.name = name;
+        this.password = password;
+        this.balance = balance;
     }
+
 }
